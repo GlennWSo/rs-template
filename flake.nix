31e3056c19c06904f6810967bfbf5d6d4e59208b
GlennWSo <sondellglenn@gmail.com>
2023-03-19 12:36:52 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust
+          ];
+
+          shellHook = ''
+            echo entering rust env
+            cargo -V
+            rustc -V
+          '';
+        };
+      }
+    );
+}
