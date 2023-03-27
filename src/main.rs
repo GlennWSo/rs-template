b03e75484ff467ac4e5f0448d61f99545f2a4a6a
GlennWSo <sondellglenn@gmail.com>
2023-03-27 09:45:59 +0200

    impl quicksort

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn bubble_sort(a: &mut Vec<i32>) {
 /// move item at i to correct position of a.sorted "pivi"
 /// items left of pivi are all smaller then a[pivi]
 /// items to the right are larger
-fn pivot(a: &mut Vec<i32>, i: usize) {
+fn pivot(a: &mut [i32], i: usize) -> usize {
     let n = a.len();
     let piv = a[i];
     a.swap(i, n - 1);
@@ -39,14 +39,26 @@ fn pivot(a: &mut Vec<i32>, i: usize) {
 
         if ileft < iright {
             a.swap(ileft, iright);
+            ileft += 1;
+            iright -= 1;
         } else {
-            a.swap(ileft, n - 1);
             break;
         }
     }
+    a.swap(ileft, n - 1);
+    ileft
 }
 
-fn quick_sort(a: &mut Vec<i32>) {}
+fn quick_sort(a: &mut [i32]) {
+    let l = a.len();
+    if l < 2 {
+        return;
+    }
+    let pivi = pivot(a, l / 2);
+    let (left, right) = a.split_at_mut(pivi);
+    quick_sort(left);
+    quick_sort(right);
+}
 
 fn main() {
     //let foo  = vec![0, 1, 2, 3, 4, 5, 6, 7];
@@ -54,10 +66,7 @@ fn main() {
     for i in 1..1 {
         println!("hello {}", i);
     }
-    let mut v1 = vec![0, 1, 3, 5, 4, 8, 7, 2];
-    bubble_sort(&mut v1);
-    println!("{:#?}", v1);
-    let mut v1 = vec![0, 1, 6, 5, 4, 8, 7, 2];
-    pivot(&mut v1, 2);
-    println!("{:#?}", v1);
+    let mut v1 = [0, 1, 6, 5, 4, 8, 7, 2];
+    quick_sort(&mut v1);
+    dbg!(v1);
 }
